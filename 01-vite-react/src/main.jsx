120b02f6e2d4fcc6f1eb370cd7d567187da15008
Harshal Jaware <jawareharshal93@gmail.com>
2025-04-09 17:18:23 +0530

    custom-react

--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <div>Custom App function</div>
+    );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+    <MyApp />
     <App />
+    </>
+    
 )
